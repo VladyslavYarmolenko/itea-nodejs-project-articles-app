16a09ad6629b8972f94d55f561e09c21fd86f0c0
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-03-06 00:18:38 +0200

    Connect Pug

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+const path = require("path");
 const express = require("express");
 const router = require("./router");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.resolve(__dirname, "views"));
 
 app.use("/", router);
 
