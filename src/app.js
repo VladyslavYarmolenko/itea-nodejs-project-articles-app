164833396158d54aae2a10a57f6982a3c716f2f0
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-03-06 02:12:08 +0200

    Define /dev router

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", path.resolve(__dirname, "views"));
 
+if (app.get("env") === "development")
+	app.use("/dev", require("./router/dev"));
+
 app.use("/", router);
 
 module.exports = app;
---
+++ src/router/dev/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+if (process.env.NODE_ENV !== "development")
+	throw new Error("Development router required on non-development environment!");
+
+const router = express.Router();
+
+router.route("/views/:filename")
+	.get((req, res) => {
+		res.render(req.params.filename, Object.assign({}, req.query, {
+			// custom values to pass
+		}));
+	});
+
+module.exports = router;
