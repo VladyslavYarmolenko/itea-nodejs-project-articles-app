bd052e0ea3e21abc8fe1fabfef86ae6a4f562dfe
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-03-06 16:32:56 +0200

    Extract mongodb cluster host to an env var

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,20 +2,23 @@ const { URL } = require("url");
 const mongoose = require("mongoose");
 
 /** @private */
-const url = new URL("mongodb+srv://cluster0.gumwa.mongodb.net?retryWrites=true&w=majority");
+const url = new URL(`mongodb+srv://${process.env.MONGODB_CLUSTER_HOST}.mongodb.net`);
 
 url.pathname = process.env.NODE_ENV;
 url.username = process.env.MONGODB_USERNAME;
 url.password = process.env.MONGODB_PASSWORD;
 
+url.searchParams.set("retryWrites", "true");
+url.searchParams.set("w", "majority");
+
 /** @public */
 async function connect() {
-	await mongoose.connect(url.toString(), {
+	const { connection } = await mongoose.connect(url.toString(), {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	});
 
-	console.log(`MongoDB connected to ${mongoose.connection.host}:${mongoose.connection.port}`);
+	console.log(`MongoDB connected to ${connection.host}:${connection.port}`);
 }
 
 module.exports = connect;
