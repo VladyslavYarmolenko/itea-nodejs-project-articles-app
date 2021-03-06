52fd83e493f87c7db266dc30980f6fa563858dc9
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-03-06 14:41:32 +0200

    Add typings

--- src/router/send-status.js
+++ src/router/send-status.js
@@ -1,4 +1,9 @@
-/** @public */
+const express = require("express");
+
+/**
+ * @public
+ * @returns {express.RequestHandler}
+ */
 const sendStatus = (status) => (req, res) => res.sendStatus(status);
 
 module.exports = sendStatus;
