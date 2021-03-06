d709693e1ba2d1b1a9cfe991742d0b442a4b84a3
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-03-06 10:39:13 +0200

    Connect mongodb

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,5 +1,19 @@
+const mongoose = require("mongoose");
+
+/** @private */
+const url = new URL("mongodb+srv://cluster0.gumwa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+url.username = process.env.MONGODB_USERNAME;
+url.password = process.env.MONGODB_PASSWORD;
+
+/** @public */
 async function connect() {
-	// TODO: ...
+	await mongoose.connect(url.toString(), {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+
+	console.log("Connected to db");
 }
 
 module.exports = connect;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const app = require("./app");
+const connect = require("./db/connect");
 
 console.log(`Starting on ${app.get("env")}...`);
 
 const port = process.env.PORT ?? 8081;
 
-app.listen(port, () => {
-	console.log(`Server is listening at http://localhost:${port}`);
+connect().then(() => {
+	app.listen(port, () => {
+		console.log(`Server is listening at http://localhost:${port}`);
+	});
 });
